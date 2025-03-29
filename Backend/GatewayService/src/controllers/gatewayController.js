9b16acf95b224b1d69d5e419549821d25c89c8fd
Commit for moving computer <aslcn7@gmail.com>
2025-03-30 02:53:43 +0300

    Add error logs to gateway service.

--- Backend/GatewayService/src/controllers/gatewayController.js
+++ Backend/GatewayService/src/controllers/gatewayController.js
@@ -8,16 +8,22 @@ const forwardAuthRequests = async (req, res, next) => {
     const path = req.originalUrl.replace('/auth', ''); 
     const url = `${authServiceUrl}${path}`;
 
+    console.log("Forwarding request to " + url, " body: " + req.body);
+    
+
     // Forward the exact method and body
     const response = await axios({
       method: req.method,
       url,
       data: req.body
     });
-
+    console.log("response: ",  JSON.stringify(response));
+    
     return res.status(response.status).json(response.data);
   } catch (error) {
     // Error from the microservice or network
+    console.log("Error while forwarding request to auth service. Error: ", error, error?.data);
+    
     if (error.response) {
       // The microservice responded with an error status
       return res.status(error.response.status).json(error.response.data);
