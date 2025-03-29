6c57f3afd434988e6d55263db1abc9bb684ee86f
Commit for moving computer <aslcn7@gmail.com>
2025-03-30 02:28:40 +0300

    Enable Cors in Gateway service

--- Backend/GatewayService/src/index.js
+++ Backend/GatewayService/src/index.js
@@ -3,11 +3,14 @@ import 'dotenv/config';
 import express, { json } from 'express';
 import { errorHandler } from './middleware/errorHandler.js';
 import gatewayRoutes from './routes/gatewayRoutes.js';
+import cors from 'cors'; 
+
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(json());
+app.use(cors());
 
 // Gateway routes
 app.use('/', gatewayRoutes);
