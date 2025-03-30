dc86e00c1e7a4a4545bac0375bcdf84529af27b5
Commit for moving computer <aslcn7@gmail.com>
2025-03-30 03:02:52 +0300

    Fix circlular json parsing

--- Backend/GatewayService/src/controllers/gatewayController.js
+++ Backend/GatewayService/src/controllers/gatewayController.js
@@ -5,11 +5,10 @@ import 'dotenv/config';
 const forwardAuthRequests = async (req, res, next) => {
   try {
     const authServiceUrl = process.env.AUTH_SERVICE_URL;
-    const path = req.originalUrl.replace('/auth', ''); 
+    const path = req.originalUrl.replace('/auth', '');
     const url = `${authServiceUrl}${path}`;
 
-    console.log("Forwarding request to " + url, " body: " + req.body);
-    
+    console.log('Forwarding request to ' + url, ' body: ' + req.body);
 
     // Forward the exact method and body
     const response = await axios({
@@ -17,13 +16,13 @@ const forwardAuthRequests = async (req, res, next) => {
       url,
       data: req.body
     });
-    console.log("response: ",  JSON.stringify(response));
-    
+    console.log('response: ', response.data);
+
     return res.status(response.status).json(response.data);
   } catch (error) {
     // Error from the microservice or network
-    console.log("Error while forwarding request to auth service. Error: ", error, error?.data);
-    
+    console.log('Error while forwarding request to auth service. Error: ', error, error?.data);
+
     if (error.response) {
       // The microservice responded with an error status
       return res.status(error.response.status).json(error.response.data);
@@ -32,6 +31,4 @@ const forwardAuthRequests = async (req, res, next) => {
   }
 };
 
-export {
-  forwardAuthRequests
-};
+export { forwardAuthRequests };
