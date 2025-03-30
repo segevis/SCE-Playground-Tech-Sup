8d456dfcd018cb9f917ead9873e2d251e288185e
Commit for moving computer <aslcn7@gmail.com>
2025-03-30 12:51:33 +0300

    Dummy commit

--- Frontend/tests/ReportsPage.js
+++ Frontend/tests/ReportsPage.js
@@ -6,11 +6,11 @@ import ReportsPage from '../src/pages/ReportsPage.jsx';
 import { Router } from 'react-router-dom';
 
 describe('HomePage', () => {
-  test('renders the welcome message', () => {
+  test('Renders the welcome message', () => {
     render(
       <Router>
         <ReportsPage />
-      </Router>
+      </Router> 
     );
     // screen is your access point to the rendered DOM
     expect(screen.getByText('Reports Page')).toBeInTheDocument();
