d3893c7281c30116692dd019ca06effd115d616d
Commit for moving computer <aslcn7@gmail.com>
2025-03-30 12:29:59 +0300

    Added initial reports page

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,24 +1,22 @@
 // frontend/src/pages/HomePage.jsx
 import React from 'react';
+import {useNavigate } from 'react-router-dom'
 
 export default function HomePage() {
+
+  const navigate = useNavigate();
+
   return (
     <div className="home-container">
-      <h1>Welcome to Our E-Commerce Store!</h1>
+      <h1>Welcome to SCE Software Ltd.</h1>
       <p>Explore our products, check your receipts, or get support. </p>
       
       <div className="home-images">
+
         <img
-          src="https://images.unsplash.com/photo-1512436991641-6745cdb1723f"
+          src="/reports.jpg"
           alt="Colorful Product 1"
-        />
-        <img
-          src="https://images.unsplash.com/photo-1503602642458-232111445657"
-          alt="Colorful Product 2"
-        />
-        <img
-          src="https://images.unsplash.com/photo-1600185365779-cdd964fba85f"
-          alt="Colorful Product 3"
+          onClick={() => navigate('/reports')}
         />
       </div>
     </div>
