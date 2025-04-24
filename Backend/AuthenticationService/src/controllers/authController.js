f347b2ddf51ecbcd710be63517210b9b06bbfaa6
Commit for moving computer <aslcn7@gmail.com>
2025-04-24 23:04:04 +0300

    Fix missing token on signIn request

--- Backend/AuthenticationService/src/controllers/authController.js
+++ Backend/AuthenticationService/src/controllers/authController.js
@@ -29,8 +29,8 @@ export async function signup(req, res, next) {
 export async function signin(req, res, next) {
   try {
     const { email, password } = req.body;
-    const user = await authService.signin(email, password);
-    return res.status(200).json({ user });
+    const userAndToken = await authService.signin(email, password);
+    return res.status(200).json(userAndToken);
   } catch (error) {
     return next(error);
   }
@@ -40,7 +40,7 @@ export async function validateToken(req, res, next) {
   try {
     const { token } = req.body;
     const isValid = await authService.validateToken(token);
-    return res.status(200).json({ valid: isValid });
+    return res.status(200).json({ isValid });
   } catch (error) {
     return next(error);
   }
