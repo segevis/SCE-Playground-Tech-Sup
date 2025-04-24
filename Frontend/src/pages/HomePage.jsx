f6868804ff984bf124bb11804fac21a2497dc79f
Commit for moving computer <aslcn7@gmail.com>
2025-04-24 18:07:19 +0300

    Add an example for how to use the user object

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,13 +1,16 @@
 // frontend/src/pages/HomePage.jsx
-import React from 'react';
+import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { StoreContext } from "../store/StoreContext";
 
 export default function HomePage() {
   const navigate = useNavigate();
+  const { user } = useContext(StoreContext);
+  console.log('user', user)
 
   return (
     <div className='home-container'>
-      <h1>Welcome to SCE Software Ltd.</h1>
+      <h1>Hi {user && user.firstName}!Welcome to SCE Software Ltd.</h1>
       <p>Explore our products, check your receipts, or get support. </p>
 
       <div className='home-images'>
